9470b48d5e77e89c513ac8103ef8c103ffd301d9
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-04-26 08:39:32 +0200

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Wrapper from "@/layout/Wrapper";
+import Home from "@/components/home-1";
+
+export const metadata = {
+  title: "De Flexijobber - Online vacaturesite voor flexwerkers in Vlaanderen",
+  description: "De Flexijobber is een platform voor werkgevers en flexwerkers in Vlaanderen die op zoek zijn naar flexibele jobs in verschillende sectoren.",
+};
+
+export default function page() {
+  return (
+    <Wrapper>
+      <Home />
+    </Wrapper>
+  );
+}
