803a15eca1cf46aa7dced44c020c7d5a8df36ab5
Safi Ur Rehman <safiurrehman74@gmail.com>
2025-04-30 23:46:14 +0500

    signup and login pages along with forgot password page

---
+++ components/common/form/forgot-password/FormContent.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+const FormContent2 = () => {
+  return (
+    <div className="form-inner">
+      <div className="text-center mb-5">
+        <Image
+          width={154}
+          height={50}
+          src="/images/logo-deflexijobber.png"
+          alt="De Flexijobber Logo"
+        />
+      </div>
+
+      {/* <!--Login Form--> */}
+      <form method="post">
+        <div className="form-group">
+          <label>Type your email to reset your password</label>
+          <input type="text" name="username" placeholder="Username" required />
+        </div>
+        {/* name */}
+
+        <div className="form-group">
+          <button
+            className="theme-btn btn-style-one"
+            type="submit"
+            name="reset-password"
+          >
+            Send Reset Link
+          </button>
+        </div>
+        {/* login */}
+      </form>
+      {/* End form */}
+
+      {/* End bottom-box LoginWithSocial */}
+    </div>
+  );
+};
+
+export default FormContent2;
