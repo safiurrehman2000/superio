a0bff1221b369dc0acae5c631563802f5b7e7eeb
safiurrehman2000 <safiurrehman74@gmail.com>
2025-05-23 18:24:20 +0500

    update

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,24 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB7__lp0oo37J-eVzncVELIZudV4ThBwQo",
-//   authDomain: "flexijobber-11a4c.firebaseapp.com",
-//   projectId: "flexijobber-11a4c",
-//   storageBucket: "flexijobber-11a4c.firebasestorage.app",
-//   messagingSenderId: "409931945808",
-//   appId: "1:409931945808:web:d20f763290e5ad92fdfe55",
-//   measurementId: "G-CT24VQ9WVR",
-// };
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
