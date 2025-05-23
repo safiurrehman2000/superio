497df3aa97c7891351a624a2383222c65bafc372
safiurrehman2000 <safiurrehman74@gmail.com>
2025-05-24 00:03:43 +0500

    fixed a small clear filter bug

--- components/job-listing-pages/components/ListingShowing.jsx
+++ components/job-listing-pages/components/ListingShowing.jsx
@@ -1,11 +1,56 @@
+"use client";
+
+import { useDispatch, useSelector } from "react-redux";
+import { setPagination } from "@/features/job/newJobSlice";
+
 const ListingShowing = () => {
+  const dispatch = useDispatch();
+  const { filteredJobs, pagination } = useSelector((state) => state.newJob);
+  const totalJobs = filteredJobs?.length || 0;
+  const showingCount = Math.min(
+    pagination.currentPage * pagination.itemsPerPage,
+    totalJobs
+  );
+  const percentage = Math.min(100, (showingCount / totalJobs) * 100);
+
+  const handleShowMore = () => {
+    // If we're about to show all jobs, set itemsPerPage to Infinity
+    if (pagination.itemsPerPage + 10 >= totalJobs) {
+      dispatch(
+        setPagination({
+          ...pagination,
+          itemsPerPage: Infinity,
+          currentPage: 1,
+        })
+      );
+    } else {
+      const newItemsPerPage = pagination.itemsPerPage + 10;
+      dispatch(
+        setPagination({
+          ...pagination,
+          itemsPerPage: newItemsPerPage,
+        })
+      );
+    }
+  };
+
+  if (pagination.itemsPerPage === Infinity || showingCount >= totalJobs) {
+    return null;
+  }
+
   return (
     <div className="ls-show-more">
-      <p>Showing 36 of 497 Jobs</p>
+      <p>
+        Showing {showingCount} of {totalJobs} Jobs
+      </p>
       <div className="bar">
-        <span className="bar-inner" style={{ width: "40%" }}></span>
+        <span className="bar-inner" style={{ width: `${percentage}%` }}></span>
       </div>
-      <button className="show-more">Show More</button>
+      <button className="show-more" onClick={handleShowMore}>
+        {totalJobs - showingCount <= 10
+          ? "Show All"
+          : `Show More (+${Math.min(10, totalJobs - showingCount)})`}
+      </button>
     </div>
   );
 };
