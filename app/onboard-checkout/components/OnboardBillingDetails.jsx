385c51be17871a631764efccec93f5948848bdaa
safiurrehman2000 <safiurrehman74@gmail.com>
2025-06-22 19:57:15 +0500

    onboarding changes

--- app/onboard-checkout/components/OnboardBillingDetails.jsx
+++ app/onboard-checkout/components/OnboardBillingDetails.jsx
@@ -5,55 +5,55 @@ import { useRouter } from "next/navigation";
 const OnboardBillingDetails = () => {
   const router = useRouter();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add form submission logic here
-    router.push("/create-profile-employer");
+  const handleSubmit = () => {
+    router.push("/onboard-order-completed");
   };
 
   return (
-    <form onSubmit={handleSubmit} className="default-form">
-      <div className="row">
-        <div className="form-group col-lg-6 col-md-12 col-sm-12">
-          <div className="field-label">
-            First name <sup>*</sup>
-          </div>
-          <input type="text" name="first_name" placeholder="" required />
+    <div className="default-form row">
+      <div className="form-group col-lg-6 col-md-12 col-sm-12">
+        <div className="field-label">
+          First name <sup>*</sup>
         </div>
-
-        <div className="form-group col-lg-6 col-md-12 col-sm-12">
-          <div className="field-label">
-            Last name <sup>*</sup>
-          </div>
-          <input type="text" name="last_name" placeholder="" required />
-        </div>
-
-        <div className="form-group col-lg-12 col-md-12 col-sm-12">
-          <div className="field-label">Company name</div>
-          <input type="text" name="company_name" placeholder="" />
-        </div>
-
-        <div className="form-group col-lg-12 col-md-12 col-sm-12">
-          <div className="field-label">
-            Email Address <sup>*</sup>
-          </div>
-          <input type="email" name="email" placeholder="" required />
-        </div>
-
-        <div className="form-group col-lg-12 col-md-12 col-sm-12">
-          <div className="field-label">
-            Phone <sup>*</sup>
-          </div>
-          <input type="tel" name="phone" placeholder="" required />
-        </div>
-
-        <div className="form-group col-lg-12 col-md-12 col-sm-12">
-          <button type="submit" className="theme-btn btn-style-one">
-            Complete Purchase
-          </button>
-        </div>
+        <input type="text" name="first_name" placeholder="" required />
       </div>
-    </form>
+
+      <div className="form-group col-lg-6 col-md-12 col-sm-12">
+        <div className="field-label">
+          Last name <sup>*</sup>
+        </div>
+        <input type="text" name="last_name" placeholder="" required />
+      </div>
+
+      <div className="form-group col-lg-12 col-md-12 col-sm-12">
+        <div className="field-label">Company name</div>
+        <input type="text" name="company_name" placeholder="" />
+      </div>
+
+      <div className="form-group col-lg-12 col-md-12 col-sm-12">
+        <div className="field-label">
+          Email Address <sup>*</sup>
+        </div>
+        <input type="email" name="email" placeholder="" required />
+      </div>
+
+      <div className="form-group col-lg-12 col-md-12 col-sm-12">
+        <div className="field-label">
+          Phone <sup>*</sup>
+        </div>
+        <input type="tel" name="phone" placeholder="" required />
+      </div>
+
+      <div className="form-group col-lg-12 col-md-12 col-sm-12">
+        <button
+          type="button"
+          onClick={handleSubmit}
+          className="theme-btn btn-style-one"
+        >
+          Complete Purchase
+        </button>
+      </div>
+    </div>
   );
 };
 
