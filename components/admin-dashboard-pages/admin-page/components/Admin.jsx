4bd14e985f092e88a46718309f8b521f3aee3c46
Safi Ur Rehman <safiurrehman74@gmail.com>
2025-07-20 17:30:46 +0500

    updated admin panel

--- components/admin-dashboard-pages/admin-page/components/Admin.jsx
+++ components/admin-dashboard-pages/admin-page/components/Admin.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import UsersTable from "./UsersTable";
 import JobsTable from "./JobsTable";
 import ApplicationsTable from "./ApplicationsTable";
+import ActiveSubscriptionsTable from "./ActiveSubscriptionsTable";
 
 export default function Admin({ loading }) {
   return (
     <div style={{ padding: 24 }}>
+      <ActiveSubscriptionsTable />
       <UsersTable loading={loading} />
       <JobsTable loading={loading} />
       <ApplicationsTable loading={loading} />
