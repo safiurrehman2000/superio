2b669fb1236e71a9805bbe5f547aeff9087cce6e
Safi Ur Rehman <safiurrehman74@gmail.com>
2025-11-15 13:32:29 +0500

    deleted unneccsary files and changed sitemap

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,6 +1,24 @@
 const siteUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://flexijobber.be";
 
-export default function sitemap() {
+function generateSitemap(routes) {
+  const urls = routes
+    .map(
+      (route) => `  <url>
+    <loc>${route.url}</loc>
+    <lastmod>${route.lastModified.toISOString()}</lastmod>
+    <changefreq>${route.changeFrequency}</changefreq>
+    <priority>${route.priority}</priority>
+  </url>`
+    )
+    .join("\n");
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+}
+
+export async function GET() {
   const routes = [
     {
       url: siteUrl,
@@ -58,5 +76,13 @@ export default function sitemap() {
     },
   ];
 
-  return routes;
+  const sitemap = generateSitemap(routes);
+
+  return new Response(sitemap, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, s-maxage=3600, stale-while-revalidate",
+    },
+  });
 }
