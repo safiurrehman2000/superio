928e526ddb4ccbe260857425ed9eff925be3002a
safiurrehman74 <safiurrehman74@gmail.com>
2026-05-02 13:52:41 +0500

    stripe address information fix

--- components/common/form/register/LoginWithSocial.jsx
+++ components/common/form/register/LoginWithSocial.jsx
@@ -1,7 +1,7 @@
 const LoginWithSocial = () => {
   return (
-    <div className="btn-box row">
-      <div className="col-lg-6 col-md-12">
+    <div className='btn-box row'>
+      {/* <div className="col-lg-6 col-md-12">
         <a href="#" className="theme-btn social-btn-two facebook-btn">
           <i className="fab fa-facebook-f"></i> Log In via Facebook
         </a>
@@ -10,7 +10,7 @@ const LoginWithSocial = () => {
         <a href="#" className="theme-btn social-btn-two google-btn">
           <i className="fab fa-google"></i> Log In via Gmail
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
